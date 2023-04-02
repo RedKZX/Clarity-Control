e182b8c86b2f8bcf1dd291c3087648a7bf6a371f
Henry <121696146+RedKZX@users.noreply.github.com>
2023-04-02 17:10:34 +0100

    add: afk code into index.js

--- src/index.js
+++ src/index.js
@@ -49,6 +49,52 @@ setInterval(() => {
 
 })
 
+// AFK System Code //
+ 
+const afkSchema = require('./Schemas.js/afkSchema');
+ 
+client.on(Events.MessageCreate, async (message) => {
+ 
+    if (message.author.bot) return;
+ 
+    const afkcheck = await afkSchema.findOne({ Guild: message.guild.id, User: message.author.id});
+    if (afkcheck) {
+        const nick = afkcheck.Nickname;
+ 
+        await afkSchema.deleteMany({
+            Guild: message.guild.id,
+            User: message.author.id
+        })
+ 
+        await message.member.setNickname(`${nick}`).catch(Err => {
+            return;
+        })
+ 
+        const m1 = await message.reply({ content: `Hey, you are **back**! Unfortunately`, ephemeral: true})
+        setTimeout(() => {
+            m1.delete();
+        }, 4000)
+    } else {
+ 
+        const members = message.mentions.users.first();
+        if (!members) return;
+        const afkData = await afkSchema.findOne({ Guild: message.guild.id, User: members.id })
+ 
+        if (!afkData) return;
+ 
+        const member = message.guild.members.cache.get(members.id);
+        const msg = afkData.Message;
+ 
+        if (message.content.includes(members)) {
+            const m = await message.reply({ content: `${member.user.tag} is currently AFK, let's keep it down.. \n> **Reason**: ${msg}`, ephemeral: true});
+            setTimeout(() => {
+                m.delete();
+                message.delete();
+            }, 4000)
+        }
+    }
+})
+
 //Invite Logger
 
 const inviteSchema = require('./Schemas.js/inviteSchema');
