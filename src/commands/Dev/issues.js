b3e00347aef924ce8a19ef8490409718b602f329
Henry <121696146+RedKZX@users.noreply.github.com>
2023-09-16 13:46:40 +0100

    Update issues.js

--- src/commands/Dev/issues.js
+++ src/commands/Dev/issues.js
@@ -5,6 +5,6 @@ module.exports = {
     .setName('issues')
     .setDescription('Submit an issue thread.'),
     async execute(interaction, client) {
-        await interaction.reply({ content: 'Create an issues thread on GitHub using this link: https://github.com/RedKZX/Mini-Red/issues' });
+        await interaction.reply({ content: 'Create an issues thread on GitHub using this link: https://github.com/RedKZX/Clarity-Control/issues' });
     }
 }
